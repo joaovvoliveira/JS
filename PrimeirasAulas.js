2682144dafa3402c0f00f586b8f0c84334efd962
Joao victor <Jvictor_oliveira@outlook.com.br>
2019-10-20 10:58:51 -0300

    Primeira Aula

--- PrimeirasAulas.js
+++ PrimeirasAulas.js
@@ -1,5 +1,21 @@
-function b(a,b){
-    return a+b;
-}
+/*var frase = "O Grêmio é o maior time do Brasil";
+console.log(frase.replace("Grêmio" , "Santos FC"));
 
-console.lob(b);
+var Carros = ['Fit' , 'Palio' , 'Onix', 'HB20'];
+console.log(Carros.push('Toro'));
+console.log(Carros.pop());
+console.log(Carros.reverse());
+console.log(Carros.toString());
+console.log(Carros.join(" - "));
+
+var user = [{nome: 'Joao', profissao: 'Estudante'},{nome: 'Patricia', profissao: 'Corretora'},{nome: 'Jéfrey', profissao: 'Motorista'}];
+console.log("A " + user[1].nome + " é uma " + user[1].profissao);
+
+for(var num = 0; num < Carros.length; num++){
+    console.log(Carros[num]);
+}
+*/
+
+function Clique(){
+    alert("Obrigado por Clicar");
+}
